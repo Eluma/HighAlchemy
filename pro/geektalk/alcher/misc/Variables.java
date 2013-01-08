4dfbb5028e50f810f68a9801eb6f96a730217ab5
Jacob Smallridge <jacobfromhf@gmail.com>
2013-01-08 09:27:41 -0500

    Revert "Initial Commit"
    This reverts commit 0e16e55c69f1399f2f380eca59e56594d5869095.

--- pro/geektalk/alcher/misc/Variables.java
+++
@@ -1,8 +0,0 @@
-package pro.geektalk.alcher.misc;
-
-public class Variables {
-	public static int startingExperience, startingLevel, alchs, slot, coinsMade;
-	public static long startTime;
-	public static boolean guiIsDone = false;
-	public static String status = "";
-}
